5ef643d0c51c9347f7c35020dd25593880f48189
Oliver Sturm <oliver@oliversturm.com>
2020-01-08 16:17:15 +0000

    sparql

--- src/node-prismjs.js
+++ src/node-prismjs.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 function uniq(arr) {
   const set = arr.reduce((set, item) => {
@@ -11,30 +11,31 @@ function uniq(arr) {
   return Object.keys(set);
 }
 
-const prismCore = "prismjs/components/prism-core";
+const prismCore = 'prismjs/components/prism-core';
 const Prism = require(prismCore);
 let initialized = false;
 
 const getPrism = (externalLangs = [], forceReinitialize = false) => {
   if (!initialized || forceReinitialize) {
     const prelude = [
-      "prism-clike",
-      "prism-markup",
-      "prism-javascript",
-      "prism-javadoclike",
-      "prism-c",
-      "prism-cpp",
-      "prism-ruby",
-      "prism-css",
-      "prism-sql",
-      "prism-t4-templating",
-      "prism-markup-templating"
+      'prism-clike',
+      'prism-markup',
+      'prism-javascript',
+      'prism-javadoclike',
+      'prism-c',
+      'prism-cpp',
+      'prism-ruby',
+      'prism-css',
+      'prism-sql',
+      'prism-t4-templating',
+      'prism-markup-templating',
+      'prism-sparql'
     ];
     const prismComponents = path.dirname(require.resolve(prismCore));
     const components = prelude.concat(
       fs
         .readdirSync(prismComponents)
-        .map(component => component.replace(/(\.min)?\.js$/, ""))
+        .map(component => component.replace(/(\.min)?\.js$/, ''))
     );
 
     const componentsSet = uniq(components);
@@ -42,7 +43,7 @@ const getPrism = (externalLangs = [], forceReinitialize = false) => {
       require(path.join(prismComponents, component))
     );
     externalLangs.forEach(item => {
-      require(item.startsWith(".") ? path.join(process.cwd(), item) : item);
+      require(item.startsWith('.') ? path.join(process.cwd(), item) : item);
     });
 
     initialized = true;
