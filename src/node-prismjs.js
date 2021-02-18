0b32152dd8502ce39e75570abfe894a1a5efc10e
Oli Sturm <oliver@oliversturm.com>
2021-02-18 12:34:39 +0000

    preload typescript for prism-jsdoc

--- src/node-prismjs.js
+++ src/node-prismjs.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function uniq(arr) {
   const set = arr.reduce((set, item) => {
@@ -11,39 +11,40 @@ function uniq(arr) {
   return Object.keys(set);
 }
 
-const prismCore = 'prismjs/components/prism-core';
+const prismCore = "prismjs/components/prism-core";
 const Prism = require(prismCore);
 let initialized = false;
 
 const getPrism = (externalLangs = [], forceReinitialize = false) => {
   if (!initialized || forceReinitialize) {
     const prelude = [
-      'prism-clike',
-      'prism-markup',
-      'prism-javascript',
-      'prism-javadoclike',
-      'prism-c',
-      'prism-cpp',
-      'prism-ruby',
-      'prism-css',
-      'prism-sql',
-      'prism-t4-templating',
-      'prism-markup-templating',
-      'prism-turtle'
+      "prism-clike",
+      "prism-markup",
+      "prism-javascript",
+      "prism-typescript",
+      "prism-javadoclike",
+      "prism-c",
+      "prism-cpp",
+      "prism-ruby",
+      "prism-css",
+      "prism-sql",
+      "prism-t4-templating",
+      "prism-markup-templating",
+      "prism-turtle",
     ];
     const prismComponents = path.dirname(require.resolve(prismCore));
     const components = prelude.concat(
       fs
         .readdirSync(prismComponents)
-        .map(component => component.replace(/(\.min)?\.js$/, ''))
+        .map((component) => component.replace(/(\.min)?\.js$/, ""))
     );
 
     const componentsSet = uniq(components);
-    componentsSet.forEach(component =>
+    componentsSet.forEach((component) =>
       require(path.join(prismComponents, component))
     );
-    externalLangs.forEach(item => {
-      require(item.startsWith('.') ? path.join(process.cwd(), item) : item);
+    externalLangs.forEach((item) => {
+      require(item.startsWith(".") ? path.join(process.cwd(), item) : item);
     });
 
     initialized = true;
